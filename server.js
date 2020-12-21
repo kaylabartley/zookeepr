6cdbb0fa6ba8c24069e661cb2f67e2137bb44b22
Green S <kaylasierrabartley@gmail.com>
2020-12-20 18:24:40 -0600

    added id param option

--- server.js
+++ server.js
@@ -31,6 +31,11 @@ function filterByQuery(query, animalsArray) {
       filteredResults = filteredResults.filter(animal => animal.name === query.name);
     }
     return filteredResults;
+
+}
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
 }
 
 app.get('/api/animals', (req, res) => {
@@ -40,6 +45,15 @@ app.get('/api/animals', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+  });
 
 /*app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
